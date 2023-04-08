db2c86f85542ad5632bf7686f02672c2aab8c637
Bríd O'Donnell <odonneb4@tcd.ie>
2023-04-08 16:45:13 +0100

    Combined the different user tables

--- app.py
+++ app.py
@@ -28,97 +28,23 @@ app.route('/register', methods=['POST'])(register)
 app.route('/login', methods=['POST'])(login)
 app.route('/logout', methods=['POST'])(logout)
 
-class BaseModel(object):
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-class UsersModel(db.Model, BaseModel):
-    __tablename__ = config.USERS_TABLE
-
-    id = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Integer)
-    sex = db.Column(db.String())
-    orientation = db.Column(db.String())
-    diet = db.Column(db.String())
-    drinks = db.Column(db.String())
-    education = db.Column(db.String())
-    ethnicity = db.Column(db.String())
-    job = db.Column(db.String())
-    location = db.Column(db.String())
-    pets = db.Column(db.String())
-    religion = db.Column(db.String())
-    sign = db.Column(db.String())
-    speaks = db.Column(db.String())
-    essay0 = db.Column(db.String())
-    essay1 = db.Column(db.String())
-    essay2 = db.Column(db.String())
-    essay3 = db.Column(db.String())
-    essay4 = db.Column(db.String())
-    essay5 = db.Column(db.String())
-    essay6 = db.Column(db.String())
-    essay7 = db.Column(db.String())
-    essay8 = db.Column(db.String())
-    essay9 = db.Column(db.String())
-    firstname = db.Column(db.String())
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-    def __init__(self, age, sex, orientation, diet, drinks, education, ethnicity, job, location, pets, religion, sign, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9, firstname):
-        self.age = age
-        self.sex = sex
-        self.orientation = orientation
-        self.diet = diet
-        self.drinks = drinks
-        self.education = education
-        self.ethnicity = ethnicity
-        self.job = job
-        self.location = location
-        self.pets = pets
-        self.religion = religion
-        self.sign = sign
-        self.speaks = speaks
-        self.essay0 = essay0
-        self.essay1 = essay1
-        self.essay2 = essay2
-        self.essay3 = essay3
-        self.essay4 = essay4
-        self.essay5 = essay5
-        self.essay6 = essay6
-        self.essay7 = essay7
-        self.essay8 = essay8
-        self.essay9 = essay9
-
-def fetch_users_data():
-    return json.dumps([u.as_dict() for u in UsersModel.query.all()], default=str)
-
-def fetch_users_data_as_list():
-    return [u.as_dict() for u in UsersModel.query.all()]
-
-def fetch_user_data_by_id(user_id):
-    return UsersModel.query.get(user_id)
-
 @app.route('/')
 def index_startpage():
     return "Welcome To Our Wingman Backend Flask Application"
 
 # Gets all the users 
-@app.route('/usersdata')
+@app.route('/all-users')
 def get_users_data():
-    return str(fetch_users_data())
+    return str(UserModel.fetch_users_data())
 
 # Gets all the users similar to a given user (using userID as a api query parameter) using K-means clustering
-@app.route('/getSimilarUsers')
+@app.route('/get-similar-users')
 def get_similar_users():
     id = request.args.get('userID')
     if not id:
         response = {'message': 'Please provide a user id at the end of URL (E.g ?userID=1)'}, 400
     else:
-         data = fetch_users_data_as_list()
+         data = UserModel.fetch_users_data_as_list()
          df = pd.DataFrame(data)
          try:
             response = KMeans.getSimilarUsers(df,id).to_json(orient = "records")
@@ -127,24 +53,19 @@ def get_similar_users():
     
     return response
 
-@app.route('/getIceBreakers')
+@app.route('/get-ice-breakers')
 def get_ice_breakers_for_user():
     id = request.args.get('userID')
     if not id:
         response = {'message': 'Please provide a user id at the end of URL (E.g ?userID=1)'}, 400
     else:
-        user = fetch_user_data_by_id(user_id=id)
+        user = UserModel.fetch_user_data_by_id(user_id=id)
         if user is not None:
             # TODO : Pass the topics to the GPT model for sentences
             response = text_features.extract_topics_per_user(user.as_dict())
         else:
             response = {'message': 'User not found.'}, 404
     return response
-
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = UserModel.query.all()
-    return jsonify([u.as_dict() for u in users])
 
 @app.route('/update-user/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
@@ -183,14 +104,14 @@ def update_user(user_id):
     if location:
         user.location = location
     if bio:
-        user.bio = bio
+        user.essay0 = bio
 
     db.session.commit()
 
     return jsonify({'message': 'User updated successfully'})
 
 @app.route('/add-swipe', methods=['POST'])
-def submit_swipe():
+def add_swipe():
     user_id = request.json.get('user_id')
     swiped_user_id = request.json.get('swiped_user_id')
     swipe_type = request.json.get('swipe_type')
--- models.py
+++ models.py
@@ -1,48 +1,94 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import config
+import json
 
 db = SQLAlchemy()
 
-class UserModel(db.Model):
-    __tablename__ = 'TestUser'
+class BaseModel(object):
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class UserModel(db.Model, BaseModel):
+    __tablename__ = config.USERS_TABLE
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     age = db.Column(db.Integer)
     sex = db.Column(db.String())
     orientation = db.Column(db.String())
     diet = db.Column(db.String())
     drinks = db.Column(db.String())
+    education = db.Column(db.String())
+    ethnicity = db.Column(db.String())
+    job = db.Column(db.String())
     location = db.Column(db.String())
-    bio = db.Column(db.String)
+    pets = db.Column(db.String())
+    religion = db.Column(db.String())
+    sign = db.Column(db.String())
+    speaks = db.Column(db.String())
+    essay0 = db.Column(db.String())
+    essay1 = db.Column(db.String())
+    essay2 = db.Column(db.String())
+    essay3 = db.Column(db.String())
+    essay4 = db.Column(db.String())
+    essay5 = db.Column(db.String())
+    essay6 = db.Column(db.String())
+    essay7 = db.Column(db.String())
+    essay8 = db.Column(db.String())
+    essay9 = db.Column(db.String())
+    firstname = db.Column(db.String())
+    name = db.Column(db.String())
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, name, email, password, age, sex, orientation, diet, drinks, location, bio):
-        self.name = name
+    def __init__(self, email, password):
         self.email = email
         self.password = password
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __init__(self, age, sex, orientation, diet, drinks, education, ethnicity, job, location, pets, religion, sign, speaks, essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9, firstname):
         self.age = age
         self.sex = sex
         self.orientation = orientation
         self.diet = diet
         self.drinks = drinks
+        self.education = education
+        self.ethnicity = ethnicity
+        self.job = job
         self.location = location
-        self.bio = bio
-
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        self.pets = pets
+        self.religion = religion
+        self.sign = sign
+        self.speaks = speaks
+        self.essay0 = essay0
+        self.essay1 = essay1
+        self.essay2 = essay2
+        self.essay3 = essay3
+        self.essay4 = essay4
+        self.essay5 = essay5
+        self.essay6 = essay6
+        self.essay7 = essay7
+        self.essay8 = essay8
+        self.essay9 = essay9
 
     def fetch_users_data():
+        return json.dumps([u.as_dict() for u in UserModel.query.all()], default=str)
+
+    def fetch_users_data_as_list():
         return [u.as_dict() for u in UserModel.query.all()]
+
+    def fetch_user_data_by_id(user_id):
+        return UserModel.query.get(user_id)
 
 class SwipeModel(db.Model):
     __tablename__ = 'swipes'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('TestUser.id'), nullable=False)
-    swiped_user_id = db.Column(db.Integer, db.ForeignKey('TestUser.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    swiped_user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     swipe_type = db.Column(db.String(10), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -55,8 +101,8 @@ class MatchModel(db.Model):
     __tablename__ = 'matches'
 
     id = db.Column(db.Integer, primary_key=True)
-    user1_id = db.Column(db.Integer, db.ForeignKey('TestUser.id'), nullable=False)
-    user2_id = db.Column(db.Integer, db.ForeignKey('TestUser.id'), nullable=False)
+    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, user1_id, user2_id):
